9cb67f548b5e5296b0114aa29da46003f1d19e8b
ericsath23 <ericsathya.23@gmail.com>
2026-07-14 12:36:17 +0700

    read notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+import { supabase } from "./supabase";
+
 export default function App() {
-  return <h1>Notes</h1>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    supabase
+      .from("notes")
+      .select("*")
+      .order("updated_at", { ascending: false })
+      .then(({ data, error }) => {
+        if (error) console.error(error);
+        else setNotes(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
